4e6fd82f8eef815087a3b8eb3b311544f0f359f3
Manuel Reigner <manuel.reigner@me.com>
2012-12-13 17:37:38 +0100

    Erstellen
    Hab mal Mainfile erstellt und bei github eingerichtet

---
+++ Schiffeversenken/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Schiffeversenken
+//
+//  Created by Manuel Reigner on 13.12.12.
+//  Copyright (c) 2012 Manuel Reigner. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
